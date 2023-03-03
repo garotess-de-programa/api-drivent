f4db1f1ccf7e204702a3f66e3cea2219b3a753a2
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-02 21:00:00 -0300

    refactor: change hotel-repository index to include available room count and hotel capacity

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -1,7 +1,31 @@
 import { prisma } from "@/config";
 
 async function findHotels() {
-  return prisma.hotel.findMany();
+  return prisma.$queryRaw`
+    WITH rooms_hotels AS (
+      SELECT
+        hotels.id,
+        COUNT(boo."userId") as users
+      FROM
+        "Hotel" hotels
+      INNER JOIN "Room" rooms ON rooms."hotelId" = hotels.id
+      INNER JOIN "Booking" boo ON boo."roomId" = rooms.id
+      GROUP BY hotels.id
+    )
+    SELECT
+      hotels.id,
+      hotels.name,
+      hotels.image,
+      MAX(rooms.capacity) AS capacity,
+      SUM(rooms.capacity) - rooms_hotels.users AS "availableRooms"
+    FROM 
+      "Room" rooms 
+    INNER JOIN "Hotel" hotels ON rooms."hotelId" = hotels.id
+    INNER JOIN rooms_hotels ON rooms_hotels.id = hotels.id
+    GROUP BY 
+      hotels.id,
+      rooms_hotels.users
+  `;
 }
 
 async function findRoomsByHotelId(hotelId: number) {
@@ -11,7 +35,7 @@ async function findRoomsByHotelId(hotelId: number) {
     },
     include: {
       Rooms: true,
-    }
+    },
   });
 }
 

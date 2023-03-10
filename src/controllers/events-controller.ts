8ad72e2adfec321ce89290f0f50e1cde88ef0b78
Júlia Botelho <cajujumo@gmail.com>
2023-03-10 16:17:45 -0300

    feat: redis EVENT data persistence implementation

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,11 +1,21 @@
 import eventsService from "@/services/events-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { redis } from "@/config";
+
+const EXPIRATION = 10000;
 
 export async function getDefaultEvent(_req: Request, res: Response) {
+  const eventkey = "event";
   try {
-    const event = await eventsService.getFirstEvent();
-    return res.status(httpStatus.OK).send(event);
+    const cachedEvent = await redis.get(eventkey);
+    if(cachedEvent){
+      return res.status(httpStatus.OK).send(JSON.parse(cachedEvent));
+    }else{
+      const event = await eventsService.getFirstEvent();
+      redis.setEx(eventkey, EXPIRATION, JSON.stringify(event));
+      return res.status(httpStatus.OK).send(event);
+    }
   } catch (error) {
     return res.status(httpStatus.NOT_FOUND).send({});
   }

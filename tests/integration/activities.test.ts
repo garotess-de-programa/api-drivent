1b434d2c543794582ee2da6ac30df1c1537258e7
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-16 00:14:28 -0300

    feat: Add integration tests for POST /activities endpoint

--- tests/integration/activities.test.ts
+++ tests/integration/activities.test.ts
@@ -174,3 +174,76 @@ describe("GET /activities", () => {
     });
   });
 });
+
+describe("POST /activities", () => {
+  const createValidBody = (id: number) => ({ activityId: id });
+  const createInvalidBody = (id: number) => ({ activityIdd: id });
+
+  it("should respond with status 401 if no token is given", async () => {
+    const validBody = createValidBody(0);
+    const response = await server.post("/activities").send(validBody);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+    const validBody = createValidBody(0);
+
+    const response = await server.post("/activities").set("Authorization", `Bearer ${token}`).send(validBody);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const validBody = createValidBody(0);
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post("/activities").set("Authorization", `Bearer ${token}`).send(validBody);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 201 with a valid body", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+
+      await createPayment(ticket.id, ticketType.price);
+
+      const createdActivity = await createActivity();
+      const validBodyWithValidIdActivity = createValidBody(createdActivity.id);
+
+      const response = await server
+        .post("/activities")
+        .set("Authorization", `Bearer ${token}`)
+        .send(validBodyWithValidIdActivity);
+
+      expect(response.status).toEqual(httpStatus.CREATED);
+    });
+
+    it("should respond with status 400 with a invalid body", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+
+      await createPayment(ticket.id, ticketType.price);
+
+      const createdActivity = await createActivity();
+      const invalidBodyWithValidIdActivity = createInvalidBody(createdActivity.id);
+
+      const response = await server
+        .post("/activities")
+        .set("Authorization", `Bearer ${token}`)
+        .send(invalidBodyWithValidIdActivity);
+
+      expect(response.status).toEqual(httpStatus.BAD_REQUEST);
+    });
+  });
+});
